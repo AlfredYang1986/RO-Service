69688bd9e1ce43f756957a8c33180832d16008dd
李伟 <850012141@qq.com>
2017-08-30 17:24:10 +0800

    Ajax+layUi Table_Page
    Ajax+layUi Table_Page

--- app/controllers/RegionalManagementController.scala
+++ app/controllers/RegionalManagementController.scala
@@ -11,7 +11,7 @@ class RegionalManagementController  extends Controller {
     def queryMultipleRegionalManagement = Action(request =>
         try {
             request.body.asJson.map { x =>
-                Ok(succesJSON(resultJson()))
+                Ok(resultJson())
             }.getOrElse (BadRequest("Bad Request for input"))
         } catch {
             case _ : Exception => BadRequest("Bad Request for input")
@@ -22,31 +22,162 @@ class RegionalManagementController  extends Controller {
         toJson(List(
             toJson(Map(
                 "id" -> toJson("1")
-                ,"hospital" -> toJson("人民医院")
-                ,"productA1" -> toJson("66,667")
-                ,"productA2" -> toJson("66,667")
-                ,"productA3" -> toJson("66,667")
-                ,"productB1" -> toJson("13,333")
-                ,"productB2" -> toJson("13,333")
-                ,"productB3" -> toJson("13,333")
-                ,"productC1" -> toJson("代表1")
-                ,"productC2" -> toJson("代表1")
-                ,"productC3" -> toJson("代表1")
+                , "hospital" -> toJson("人民医院")
+                , "productA1" -> toJson("66,667")
+                , "productA2" -> toJson("66,667")
+                , "productA3" -> toJson("66,667")
+                , "productB1" -> toJson("13,333")
+                , "productB2" -> toJson("13,333")
+                , "productB3" -> toJson("13,333")
+                , "productC1" -> toJson("代表1")
+                , "productC2" -> toJson("代表1")
+                , "productC3" -> toJson("代表1")
+            )),
+            toJson(Map(
+                "id" -> toJson("2")
+                , "hospital" -> toJson("协和医院")
+                , "productA1" -> toJson("66,667")
+                , "productA2" -> toJson("66,667")
+                , "productA3" -> toJson("66,667")
+                , "productB1" -> toJson("13,333")
+                , "productB2" -> toJson("13,333")
+                , "productB3" -> toJson("13,333")
+                , "productC1" -> toJson("代表1")
+                , "productC2" -> toJson("代表1")
+                , "productC3" -> toJson("代表1")
+            )),
+            toJson(Map(
+                "id" -> toJson("3")
+                , "hospital" -> toJson("肿瘤医院")
+                , "productA1" -> toJson("66,667")
+                , "productA2" -> toJson("66,667")
+                , "productA3" -> toJson("66,667")
+                , "productB1" -> toJson("13,333")
+                , "productB2" -> toJson("13,333")
+                , "productB3" -> toJson("13,333")
+                , "productC1" -> toJson("代表1")
+                , "productC2" -> toJson("代表1")
+                , "productC3" -> toJson("代表1")
+            )),
+            toJson(Map(
+                "id" -> toJson("4")
+                , "hospital" -> toJson("妇产医院")
+                , "productA1" -> toJson("66,667")
+                , "productA2" -> toJson("66,667")
+                , "productA3" -> toJson("66,667")
+                , "productB1" -> toJson("13,333")
+                , "productB2" -> toJson("13,333")
+                , "productB3" -> toJson("13,333")
+                , "productC1" -> toJson("代表1")
+                , "productC2" -> toJson("代表1")
+                , "productC3" -> toJson("代表1")
+            )),
+            toJson(Map(
+                "id" -> toJson("5")
+                , "hospital" -> toJson("社区医院")
+                , "productA1" -> toJson("66,667")
+                , "productA2" -> toJson("66,667")
+                , "productA3" -> toJson("66,667")
+                , "productB1" -> toJson("13,333")
+                , "productB2" -> toJson("13,333")
+                , "productB3" -> toJson("13,333")
+                , "productC1" -> toJson("代表1")
+                , "productC2" -> toJson("代表1")
+                , "productC3" -> toJson("代表1")
             ))
+//            ,
+//            toJson(Map(
+//                "id" -> toJson("1")
+//                , "hospital" -> toJson("人民医院")
+//                , "productA1" -> toJson("66,667")
+//                , "productA2" -> toJson("66,667")
+//                , "productA3" -> toJson("66,667")
+//                , "productB1" -> toJson("13,333")
+//                , "productB2" -> toJson("13,333")
+//                , "productB3" -> toJson("13,333")
+//                , "productC1" -> toJson("代表1")
+//                , "productC2" -> toJson("代表1")
+//                , "productC3" -> toJson("代表1")
+//            )),
+//            toJson(Map(
+//                "id" -> toJson("6")
+//                , "hospital" -> toJson("协和医院")
+//                , "productA1" -> toJson("66,667")
+//                , "productA2" -> toJson("66,667")
+//                , "productA3" -> toJson("66,667")
+//                , "productB1" -> toJson("13,333")
+//                , "productB2" -> toJson("13,333")
+//                , "productB3" -> toJson("13,333")
+//                , "productC1" -> toJson("代表1")
+//                , "productC2" -> toJson("代表1")
+//                , "productC3" -> toJson("代表1")
+//            )),
+//            toJson(Map(
+//                "id" -> toJson("7")
+//                , "hospital" -> toJson("肿瘤医院")
+//                , "productA1" -> toJson("66,667")
+//                , "productA2" -> toJson("66,667")
+//                , "productA3" -> toJson("66,667")
+//                , "productB1" -> toJson("13,333")
+//                , "productB2" -> toJson("13,333")
+//                , "productB3" -> toJson("13,333")
+//                , "productC1" -> toJson("代表1")
+//                , "productC2" -> toJson("代表1")
+//                , "productC3" -> toJson("代表1")
+//            )),
+//            toJson(Map(
+//                "id" -> toJson("8")
+//                , "hospital" -> toJson("妇产医院")
+//                , "productA1" -> toJson("66,667")
+//                , "productA2" -> toJson("66,667")
+//                , "productA3" -> toJson("66,667")
+//                , "productB1" -> toJson("13,333")
+//                , "productB2" -> toJson("13,333")
+//                , "productB3" -> toJson("13,333")
+//                , "productC1" -> toJson("代表1")
+//                , "productC2" -> toJson("代表1")
+//                , "productC3" -> toJson("代表1")
+//            )),
+//            toJson(Map(
+//                "id" -> toJson("9")
+//                , "hospital" -> toJson("社区医院")
+//                , "productA1" -> toJson("66,667")
+//                , "productA2" -> toJson("66,667")
+//                , "productA3" -> toJson("66,667")
+//                , "productB1" -> toJson("13,333")
+//                , "productB2" -> toJson("13,333")
+//                , "productB3" -> toJson("13,333")
+//                , "productC1" -> toJson("代表1")
+//                , "productC2" -> toJson("代表1")
+//                , "productC3" -> toJson("代表1")
+//            )),
+//            toJson(Map(
+//                "id" -> toJson("10")
+//                , "hospital" -> toJson("人民医院")
+//                , "productA1" -> toJson("66,667")
+//                , "productA2" -> toJson("66,667")
+//                , "productA3" -> toJson("66,667")
+//                , "productB1" -> toJson("13,333")
+//                , "productB2" -> toJson("13,333")
+//                , "productB3" -> toJson("13,333")
+//                , "productC1" -> toJson("代表1")
+//                , "productC2" -> toJson("代表1")
+//                , "productC3" -> toJson("代表1")
+//            )),
+//            toJson(Map(
+//                "id" -> toJson("11")
+//                , "hospital" -> toJson("协和医院")
+//                , "productA1" -> toJson("66,667")
+//                , "productA2" -> toJson("66,667")
+//                , "productA3" -> toJson("66,667")
+//                , "productB1" -> toJson("13,333")
+//                , "productB2" -> toJson("13,333")
+//                , "productB3" -> toJson("13,333")
+//                , "productC1" -> toJson("代表1")
+//                , "productC2" -> toJson("代表1")
+//                , "productC3" -> toJson("代表1")
+//            ))
+
         ))
-    }
-
-    def succesJSON(data : JsValue) : JsValue = {
-        toJson(Map("code" -> toJson(0),
-            "msg" -> toJson("成功"),
-            "count" -> toJson(10),
-            "data" -> data))
-    }
-
-    def failedJSON(data : JsValue) : JsValue = {
-        toJson(Map("code" -> toJson(1),
-            "msg" -> toJson("失败"),
-            "count" -> toJson(10),
-            "data" -> data))
     }
 }
--- public/ro/js/ro-regional-management.js
+++ public/ro/js/ro-regional-management.js
@@ -1,14 +1,42 @@
 /**
  * Created by liwei on 2017/8/21.
  */
-layui.use('table', function(){
+layui.use('table', function() {
     var table = layui.table;
-});
-
-$(function () {
+    //方法级渲染
     var dataMap = JSON.stringify({});
     AjaxData("/regionalManagement/list", dataMap, "POST", function (data) {
-        console.info(data);
+        window.demoTable = table.render({elem: '#rm'
+            ,data : data
+            //, width: 1100
+            , cols: [
+                [
+                    {space: true, fixed: false, rowspan: '2', title: '#'},
+                    {field:'hospital', width:150, align:'center', rowspan:'2', title: '目标医院'},
+                    {align:'center', colspan:'3', title: '销售指标'},
+                    {align:'center', colspan:'3', title: '费用'},
+                    {align:'center', colspan:'3', title: '代表'}
+                ],
+                [
+                    {field:'productA1', width:100, edit: 'text', title: '产品A'}
+                    , {field:'productA2', width:100, edit: 'text', title: '产品B'}
+                    , {field:'productA3', width:100, edit: 'text', title: '产品C'}
+                    , {field:'productB1', width:100, edit: 'text', title: '产品A'}
+                    , {field:'productB2', width:100, edit: 'text', title: '产品B'}
+                    , {field:'productB3', width:100, edit: 'text', title: '产品C'}
+                    , {field:'productC1', width:100, edit: 'text', title: '产品A'}
+                    , {field:'productC2', width:100, edit: 'text', title: '产品B'}
+                    , {field:'productC3', width:100, edit: 'text', title: '产品C'}
+                ]
+            ]
+            , skin: 'row'
+            , even: true
+            //, size: 'lg'
+            , page: true
+            , limits: [10, 20, 50]
+            , limit: 10
+            //, loading: false
+        });
     }, function (err) {
         console.log(err.name);
     })
